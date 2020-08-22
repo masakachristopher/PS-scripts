47ba98b6183e715437c81dd45081a7a1ef802937
masakachristopher <masakachristopher@yahoo.com>
2020-08-23 02:18:53 +0300

    Delete trashcleaner.ps1

--- trashcleaner.ps1
+++
@@ -1,8 +0,0 @@
-﻿$days = 30
-$now = Get-Date
-$restriction = $now.AddDays(-$days)
-$dir = "C:\`$Recycle.Bin"
-//remove files
-Get-ChildItem $dir -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $restriction } | Remove-Item -Force
-//remove empty folders
-Get-ChildItem $dir -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
