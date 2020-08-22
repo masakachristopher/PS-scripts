f4d1f747c3e118d9e9c11941dae72714941ff842
masakachristopher <masakachristopher@yahoo.com>
2020-08-23 01:10:11 +0300

    initial upload

---
+++ trashcleaner.ps1
@@ -0,0 +1,8 @@
+﻿$days = 30
+$now = Get-Date
+$restriction = $now.AddDays(-$days)
+$dir = "C:\`$Recycle.Bin"
+//remove files
+Get-ChildItem $dir -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $restriction } | Remove-Item -Force
+//remove empty folders
+Get-ChildItem $dir -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
